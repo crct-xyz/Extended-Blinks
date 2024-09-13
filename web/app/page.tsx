4b042c3853b1734c88cd1bcf1270b9302622f924
Eloi <es@gsmk.de>
2024-09-13 19:07:54 +0200

    add select and input component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,50 +1,98 @@
-"use client";import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import React, { useState, FormEvent } from 'react';
+"use client";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+
+import type React from "react";
+import { useState, FormEvent } from "react";
 
 export default function Page() {
-  const [address, setAddress] = useState("");
-  const baseUrl = process.env.NODE_ENV === 'production'
-    ? 'https://extended-blinks.vercel.app'
-    : 'http://localhost:3000';
+	const [address, setAddress] = useState("");
+	const baseUrl =
+		process.env.NODE_ENV === "production"
+			? "https://extended-blinks.vercel.app"
+			: "http://localhost:3000";
 
-  const handleAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAddress(event.target.value)
-  }
+	const handleAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setAddress(event.target.value);
+	};
 
-  const handleRedirection = () => {
+	const handleRedirection = () => {};
 
-  }
+	const SelectMenu = ({ values }: { values?: string[] }) => {
+		return (
+			<Select>
+				<SelectTrigger className="w-[180px]">
+					<SelectValue placeholder="" />
+				</SelectTrigger>
+				<SelectContent style={{ backgroundColor: "white" }}>
+					{values?.map((value) => {
+						return (
+							<SelectItem key={value} value={value}>
+								{value}
+							</SelectItem>
+						);
+					})}
+				</SelectContent>
+			</Select>
+		);
+	};
+	// https://dial.to/?action=solana-action%3A${baseUrl}/api/action/approve?squad=${address}`
 
-  // https://dial.to/?action=solana-action%3A${baseUrl}/api/action/approve?squad=${address}`
+	return (
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			<input type="text" onChange={handleAddress} />
+			<a
+				href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad?address=${address}`}
+			>
+				Make transaction
+			</a>
+			<a
+				href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/config?address=${address}`}
+			>
+				Config wallet
+			</a>
+			<a
+				href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/deposit?address=${address}`}
+			>
+				Deposit
+			</a>
+			<a
+				href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/vote?address=${address}`}
+			>
+				vote on a given transaction
+			</a>
+			<Card style={{ maxWidth: "300px" }}>
+				<CardHeader>
+					<CardTitle>What kind of transaction do you want to make?</CardTitle>
+					<CardDescription>
+						<SelectMenu values={["Approve", "Deposit", "Vote"]} />
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<p>Which kind of transaction you wanna get triggered?</p>
+					<SelectMenu values={["Approve", "Deposit", "Vote"]} />
+				</CardContent>
+				<CardFooter>
+					<p>Card Footer</p>
+					<Input />
+				</CardFooter>
+			</Card>
 
-  return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
-      <input type="text" onChange={handleAddress}  />
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad?address=${address}`}>Make transaction</a>
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/config?address=${address}`}>Config wallet</a>
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/deposit?address=${address}`}>Deposit</a>
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/vote?address=${address}`}>vote on a given transaction</a>
-      <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-
-      <a href="/bot">bot</a>
-    </div>
-  )
+			<a href="/bot">bot</a>
+		</div>
+	);
 }
