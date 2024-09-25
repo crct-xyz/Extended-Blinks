6e171bae99b5c9ec36f2d348b1657fb761b76210
Eloi <es@gsmk.de>
2024-09-25 12:07:16 +0200

    Keep calm and commit

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,31 +1,28 @@
 "use client";
-import React, { useState, FormEvent } from 'react';
+import LandingAnimation from "@/components/landing-animation/landing-animation";
+import type React from "react";
+import { useState, FormEvent } from "react";
 
 export default function Page() {
-  const [address, setAddress] = useState("");
-  const baseUrl = process.env.NODE_ENV === 'production'
-    ? 'https://extended-blinks.vercel.app'
-    : 'http://localhost:3000';
+	const [address, setAddress] = useState("");
+	const baseUrl =
+		process.env.NODE_ENV === "production"
+			? "https://extended-blinks.vercel.app"
+			: "http://localhost:3000";
 
-  const handleAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAddress(event.target.value)
-  }
+	const handleAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setAddress(event.target.value);
+	};
 
-  const handleRedirection = () => {
+	const handleRedirection = () => {};
 
-  }
+	// https://dial.to/?action=solana-action%3A${baseUrl}/api/action/approve?squad=${address}`
 
-  // https://dial.to/?action=solana-action%3A${baseUrl}/api/action/approve?squad=${address}`
+	return (
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			<LandingAnimation />
 
-  return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
-      <input type="text" onChange={handleAddress}  />
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad?address=${address}`}>Make transaction</a>
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/config?address=${address}`}>Config wallet</a>
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/deposit?address=${address}`}>Deposit</a>
-      <a href={`https://dial.to/?action=solana-action%3A${baseUrl}/api/actions/squad/vote?address=${address}`}>vote on a given transaction</a>
-
-      <a href="/bot">bot</a>
-    </div>
-  )
+			<a href="/bot">bot</a>
+		</div>
+	);
 }
