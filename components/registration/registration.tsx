a0d0dd2a74cf6f03ef02ddc530fcae0171ab40b9
Eloi <es@gsmk.de>
2024-10-03 22:06:12 +0200

    add labels

--- components/registration/registration.tsx
+++ components/registration/registration.tsx
@@ -2,26 +2,24 @@ const RegistrationComp = () => {
     return (
         <div className="xs:w-96 flex w-auto flex-col gap-5">
             <div className="flex flex-col gap-5 rounded-lg border-2 border-solid bg-[#837e7e] p-6 text-center">
-                <span className="font-bold text-black">USERNAME</span>
-                <input
-                    type="text"
-                    placeholder="PUBKEY"
-                    className="rounded-md px-1 placeholder:p-2 placeholder:text-black"
-                />
-            </div>
-            <div className="flex flex-col gap-5 rounded-lg border-2 border-solid bg-[#837e7e] p-6 text-center">
                 <span className="font-bold text-black">
                     HOW DO YOU WANT TO GET NOTIFIED?
                 </span>
-                <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-3 text-start">
+                    <label htmlFor="telegram">Telegram</label>
                     <input
                         type="text"
-                        placeholder="TELEGRAM"
+                        id="telegram"
+                        name="telegram"
+                        placeholder="Enter your Telegram handle"
                         className="rounded-md px-1 placeholder:p-2 placeholder:text-black"
                     />
+                    <label htmlFor="email">Email</label>
                     <input
                         type="text"
-                        placeholder="X"
+                        id="email"
+                        name="email"
+                        placeholder="Enter your email..."
                         className="rounded-md px-1 placeholder:p-2 placeholder:text-black"
                     />
                 </div>
