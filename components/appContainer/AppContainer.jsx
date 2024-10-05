3dfde1b3617bdf8014b0ae90485062646f42555d
Aarsho <chatterjeeaarsho@gmail.com>
2024-10-05 05:08:22 +0200

    usdc use case

--- components/appContainer/AppContainer.jsx
+++ components/appContainer/AppContainer.jsx
@@ -3,15 +3,20 @@ import JupyterIcon from 'components/icons/jupyter-icon'
 import SquadsIcon from 'components/icons/squads-icon'
 import TensorIcon from 'components/icons/tensor-icon'
 import SquadsUseCases from 'components/squadsUseCasesContainer/SquadsUseCases'
+import USDCuseCases from 'components/USDCuseCases/USDCuseCases'
 import React, { useState } from 'react'
 
 function AppContainer() {
     const [showSquads, setShowSquads] = useState(false);
+    const [showUSDC, setShowUSDC] = useState(false);
 
     // Function to handle button click
     const handleSquadsClick = () => {
         setShowSquads(prevState => !prevState); // Show SquadsUseCases
     };
+    const handleUSDCClick = () => {
+        setShowUSDC(prevState => !prevState);
+    }
     return (
         <div className="flex flex-col items-center justify-center text-center">
             <div className="flex flex-col text-center">
@@ -36,9 +41,17 @@ function AppContainer() {
                     >
                         TENSOR
                     </button>
+                    <button
+                        type="button"
+                        className={`rounded-lg ${showUSDC ? 'bg-[#00CED1]' : 'bg-[#D9D9D9]'}`}
+                        onClick={handleUSDCClick}
+                    >
+                        USDC
+                    </button>
                 </div>
             </div>
             {showSquads && <SquadsUseCases />}
+            {showUSDC && <USDCuseCases />}
         </div>
     )
 }
