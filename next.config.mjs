c11b27a2d1ff485ada5d9f7e847c56130c5d61a5
Eloi <es@gsmk.de>
2024-10-06 11:19:47 +0200

    rewrite in config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {"source": "/", "destination": "http://ec2-52-59-228-70.eu-central-1.compute.amazonaws.com:8000/" },
+    ]
+  }
+};
 
 export default nextConfig;
